8bad1882377769f4514560f1af37451bca1e5267
buinhatminh@pc.com <buinhatminh.com>
2025-04-24 11:04:13 +0200

    more questions, question separate from main

--- main.js
+++ main.js
@@ -1,38 +1,3 @@
-const questions = [
-    {
-        question: "Was bedeutet 'if' in einer Programmiersprache?",
-        answers: [
-            "Es startet eine Schleife",
-            "Es ist eine Bedingung",
-            "Es beendet das Programm",
-            "Es definiert eine Funktion"
-        ],
-        correct: "Es ist eine Bedingung"
-    },
-
-    {
-        question: "Was ist eine Variable in der Programmierung?",
-        answers: [
-            "Eine wiederverwendbare Funktion",
-            "Ein fester Wert, der sich nicht ändert",
-            "Ein Speicherplatz für Daten",
-            "Ein Fehler im Code"
-        ],
-        correct: "Ein Speicherplatz für Daten"
-    },
-
-    {
-        question: "Was ist eine Schleife (Loop) in der Programmierung?",
-        answers: [
-            "Ein Fehler im Programm",
-            "Eine Funktion zur Bildbearbeitung",
-            "Eine Wiederholung von Anweisungen",
-            "Eine Art Kommentar"
-        ],
-        correct: "Eine Wiederholung von Anweisungen"
-    }
-];
-
 let currentQuestion = 0;
 let score = 0;
 
---
+++ questions.js
@@ -0,0 +1,45 @@
+const questions = [
+    {
+        question: "Was bedeutet 'if' in einer Programmiersprache?",
+        answers: [
+            "Es startet eine Schleife",
+            "Es ist eine Bedingung",
+            "Es beendet das Programm",
+            "Es definiert eine Funktion"
+        ],
+        correct: "Es ist eine Bedingung"
+    },
+
+    {
+        question: "Was ist eine Variable in der Programmierung?",
+        answers: [
+            "Eine wiederverwendbare Funktion",
+            "Ein fester Wert, der sich nicht ändert",
+            "Ein Speicherplatz für Daten",
+            "Ein Fehler im Code"
+        ],
+        correct: "Ein Speicherplatz für Daten"
+    },
+
+    {
+        question: "Was ist eine Schleife (Loop) in der Programmierung?",
+        answers: [
+            "Ein Fehler im Programm",
+            "Eine Funktion zur Bildbearbeitung",
+            "Eine Wiederholung von Anweisungen",
+            "Eine Art Kommentar"
+        ],
+        correct: "Eine Wiederholung von Anweisungen"
+    },
+
+    {
+        question: "Wofür steht HTML?",
+        answers: [
+            "Hyper Transfer Main Logic",
+            "High Text Machine Language",
+            "Hypertext Markup Language",
+            "High-level Markup Language"
+        ],
+        correct: "Hypertext Markup Language"
+    },
+];
